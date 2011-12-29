7687e4b5ce3b493e0c9cb5c2b85e6449c4eb46e0
Huckjay <huckjaywai@gmail.com>
2011-12-29 17:28:21 +0000

    Made changes to layout links spec

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,4 +27,23 @@ describe "LayoutLinks" do
 		response.should have_selector('title', :content => "Sign up")
 	end
 
+
+#Excercise 5.5
+#You may have noticed that our tests for the layout links test the routing but don’t actually check that the links on the layout go to the right pages. One way to implement these tests is to use visit and click_link inside the RSpec integration test.
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+
 end
